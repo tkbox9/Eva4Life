a44b2bad66722e3ed083ab1c5474162eb5988a55
T <tkbox9@gmail.com>
2018-10-01 09:09:29 +0100

    test1

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ class BasicBot {
             break;
             case DATA_PRIVACY:
                 await context.sendActivity(`Oh I see, data is very important and we...`);
-                await context.sendActivity(`2222`);
+                await context.sendActivity(`No worries`);
             break;            
             case HELP_INTENT:
                 await context.sendActivity(`Let me try to provide some help.`);
